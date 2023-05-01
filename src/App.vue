c92d434ed9fcc03848d26d4d98f534114fc2b372
“johnnyung91” <johnnyung91@gmail.com>
2023-05-01 12:27:27 -0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>hello world</h2>
+</template>
+
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+</style>
